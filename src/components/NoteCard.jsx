d5a8858cfa81ff82f959e4c7a302cd3384b18fe0
Peter Bagi <peti.bagi@icloud.com>
2024-07-25 17:39:10 +0200

    appwrite connection succasse

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,18 +1,55 @@
-import {useRef, useEffect} from 'react'
+import {useRef, useEffect, useState} from 'react'
+import { setNewOffset, autoGrow, setZIndex, bodyParser } from '../util';
 import Trash from '../icons/Trash'
 
 const NoteCard = ({note}) => {
-    const position = JSON.parse(note.position);
+   const [position, setPositon] = useState(JSON.parse(note.position));
     const colors = JSON.parse(note.colors);
-    const body = JSON.parse(note.body);
+    const body = bodyParser(note.body);
+
+
+    let mouseStartPos = { x: 0, y: 0 };
+ 
+    const cardRef = useRef(null);
 
     const textAreaRef = useRef(null);
 
-const autoGrow = (textAreaRef) => {
-  const {current} = textAreaRef;
 
-    current.style.height = "auto"; // Reset the height
-    current.style.height = current.scrollHeight + "px"; // Set the new height
+
+const mouseUp = () => {
+  document.removeEventListener("mousemove", mouseMove);
+  document.removeEventListener("mouseup", mouseUp);
+};
+
+const mouseDown = (e) => {
+  mouseStartPos.x = e.clientX;
+  mouseStartPos.y = e.clientY;
+
+  document.addEventListener("mousemove", mouseMove);
+  document.addEventListener("mouseup", mouseUp);
+
+  setZIndex(cardRef.current);
+};
+
+
+const mouseMove = (e) => {
+  let mouseMoveDir = {
+    x: mouseStartPos.x - e.clientX,
+    y: mouseStartPos.y - e.clientY,
+  };
+
+
+  mouseStartPos.x = e.clientX;
+  mouseStartPos.y = e.clientY;
+
+  const newSetPosition = setNewOffset(cardRef.current, mouseMoveDir);
+  setPositon(newSetPosition);
+
+
+  setPositon({
+    x: cardRef.current.offsetLeft - mouseMoveDir.x,
+    y: cardRef.current.offsetTop - mouseMoveDir.y,
+  });
 };
 
 useEffect(() => {
@@ -20,24 +57,30 @@ useEffect(() => {
 }, [])
 
   return (
-    <div  className='card'
-    style={{
-      backgroundColor: colors.colorBody,
-      left: `${position.x}px`,
-      top: `${position.y}px`,
-  }}>
+    <div 
+      ref={cardRef}
+     className='card'
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+    }}>
     <div
       className="card-header" 
-      style={{ backgroundColor: colors.colorHeader }}>
+      style={{ backgroundColor: colors.colorHeader }}
+      onMouseDown={mouseDown}>
             <Trash />
         </div> 
     <div className="card-body">
-      <textarea
+      <textarea  
       ref={textAreaRef} 
        style={{ color: colors.colorText }}
        defaultValue={body}
        onInput={() => {
         autoGrow(textAreaRef)
+       }}
+       onFocus={() => {
+        setZIndex(cardRef.current);
        }}
       >
 
--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import { fakeData as notes} from '../assets/fakeData.js'
+import {useState, useEffect} from 'react'
+import { databases } from '../appwrite/config.js';
 import NoteCard from '../components/NoteCard.jsx'
 
 function NotesPage() {
+
+    const [notes, setNotes] = useState([]);
+
+
+    useEffect(() => {
+        init()
+    }, [])
+
+    const init = async () => {
+        const response = await databases.listDocuments(
+            import.meta.env.VITE_DATABASE_ID,
+            import.meta.env.VITE_COLLECTION_NOTES_ID,
+        );
+
+        setNotes(response.documents);
+    }
+
+
   return (
     <div>
         {notes.map((note) => (
